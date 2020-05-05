174525b90b3a0f18774ab42fc73ef967b68cbdef
Hendrikto <hendrik.to@gmail.com>
2020-05-05 20:48:05 +0200

    Implement function to read the calendar

--- m5/utility.py
+++ m5/utility.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+calendar_dtypes = {
+    'event_name_1': 'string',
+    'event_name_2': 'string',
+    'event_type_1': 'string',
+    'event_type_2': 'string',
+    'month': 'category',
+    'snap_CA': 'bool',
+    'snap_TX': 'bool',
+    'snap_WI': 'bool',
+    'wday': 'category',
+    'year': 'category',
+}
+
 sales_data_dtypes = {
     'id': 'string',
     **{column: 'category' for column in (
@@ -11,6 +24,24 @@ sales_data_dtypes = {
     )},
     **{f'd_{i}': 'int16' for i in range(1, 1942)},
 }
+
+
+def read_calendar(path):
+    calendar = pd.read_csv(path, dtype=calendar_dtypes, usecols=calendar_dtypes)
+
+    event_names = set(
+        pd.unique(calendar[['event_name_1', 'event_name_2']].values.ravel()),
+    ) - {pd.NA}
+    calendar.event_name_1 = pd.Categorical(calendar.event_name_1, categories=event_names)
+    calendar.event_name_2 = pd.Categorical(calendar.event_name_2, categories=event_names)
+
+    event_types = set(
+        pd.unique(calendar[['event_type_1', 'event_type_2']].values.ravel()),
+    ) - {pd.NA}
+    calendar.event_type_1 = pd.Categorical(calendar.event_type_1, categories=event_types)
+    calendar.event_type_2 = pd.Categorical(calendar.event_type_2, categories=event_types)
+
+    return calendar
 
 
 def read_sales_data(path):
