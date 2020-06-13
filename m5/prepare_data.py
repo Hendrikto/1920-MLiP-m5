99939c959ff3286e50ff00410f00971c701af2a9
Hendrikto <hendrik.to@gmail.com>
2020-06-13 12:34:37 +0200

    Include sell prices in prediction data

--- m5/prepare_data.py
+++ m5/prepare_data.py
@@ -67,7 +67,7 @@ def generate_data_frame(sales_data, calendar, prices):
     return data
 
 
-def generate_prediction_data(sales_data, calendar, days):
+def generate_prediction_data(sales_data, calendar, prices, days):
     """
     Generate prediction data.
 
@@ -75,21 +75,24 @@ def generate_prediction_data(sales_data, calendar, days):
     :param calendar: dataframe with calendar information
     :param days: 0-based range of days to predict
 
-    :returns: array with generated prediction data
+    :returns: dataframe with generated prediction data
     """
     n_days = len(days)
-    n_features = len(config.feature_names) - 1
     n_products = len(sales_data)
 
-    data = np.empty((n_days * n_products, n_features), dtype='int16')
-    data[:, config.feature_index['day']] = np.repeat(days, n_products) + 1  # sales_data is 1-based
-    for column_name, column in calendar.loc[days].items():
-        if column.dtype == 'bool':
-            values = column
-        else:
-            values = column.cat.codes
-        data[:, config.feature_index[column_name]] = np.repeat(values, n_products)
+    data = {}
+
+    data['day'] = np.repeat(np.arange(days.start, days.stop, dtype='int16'), n_products)
     for column_name, column in sales_data.loc[:, 'item_id':'state_id'].items():
-        data[:, config.feature_index[column_name]] = np.tile(column.cat.codes, n_days)
+        data[column_name] = pd.concat((column,) * n_days, copy=False, ignore_index=True)
+    for column_name, column in calendar.loc[days].items():
+        data[column_name] = column.repeat(n_products).reset_index(drop=True)
+    data['wm_yr_wk'] = calendar.wm_yr_wk[:n_days].repeat(n_products).reset_index(drop=True)
+
+    data = pd.DataFrame(data)
+
+    data = data.merge(prices, how='left', on=['item_id', 'store_id', 'wm_yr_wk'])
+    data.sell_price = data.sell_price.astype('float32')
+    data.drop(columns=('wm_yr_wk'), inplace=True)
 
     return data
