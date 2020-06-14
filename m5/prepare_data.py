a94c3ca83aaca3f2130a76c50f61e457e5359b75
Hendrikto <hendrik.to@gmail.com>
2020-06-14 15:17:04 +0200

    Remove unused code

--- m5/prepare_data.py
+++ m5/prepare_data.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 from tqdm.notebook import tqdm
 
-import config
-
 
 def calculate_rolling_mean(data, n_products, window_size=28):
     column = f'rmean{window_size}_sales'
@@ -28,32 +26,6 @@ def cat2int(data_frame):
         elif t.name == 'bool':
             column = column.astype('int8')
         data_frame[column_name] = column
-
-
-def generate_data(sales_data, calendar, dtype='float32'):
-    n_days = int(sales_data.columns[-1].split('_')[1])
-    n_features = len(config.feature_index)
-    n_products = len(sales_data)
-
-    data = np.empty((n_products * n_days, n_features), dtype=dtype)
-
-    data[:, config.feature_index['day']] = np.repeat(
-        np.arange(1, n_days + 1, dtype='int16'),
-        n_products,
-    )
-    data[:, config.feature_index['sales']] = np.concatenate([
-        sales_data[f'd_{i}'] for i in range(1, n_days + 1)
-    ])
-    for column_name, column in sales_data.loc[:, 'item_id':'state_id'].items():
-        data[:, config.feature_index[column_name]] = np.tile(column.cat.codes, n_days)
-    for column_name, column in calendar.loc[:n_days - 1, 'wday':'snap_WI'].items():
-        if column.dtype == 'bool':
-            values = column
-        else:
-            values = column.cat.codes
-        data[:, config.feature_index[column_name]] = np.repeat(values, n_products)
-
-    return data
 
 
 def generate_data_frame(sales_data, calendar, prices):
